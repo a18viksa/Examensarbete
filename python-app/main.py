5f7718a03cb560aa3e54fecbf1146471011aa7b8
a18viksa <a18viksa@student.his.se>
2021-03-22 14:57:02 +0100

    Linjediagram
    Skapat exempeldata för att testa hur det funkar i diagrammet. Samt även skapat diagrammet.

--- python-app/main.py
+++ python-app/main.py
@@ -1,1 +1,11 @@
+import plotly_express as px
+import pandas as pd
+import streamlit as st
+
+stock_data = pd.read_csv("data/data.csv")
+print(stock_data.head())
+
+stock_line_chart = px.line(x='Datapoints', y='Price', data_frame=stock_data,
+                           title='Stock Line Chart', color='Name')
+st.plotly_chart(stock_line_chart)
 
