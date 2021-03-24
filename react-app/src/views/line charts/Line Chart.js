3400154b83598f6b847a43def94cf98596cf5dcc
a18viksa <a18viksa@student.his.se>
2021-03-24 17:36:43 +0100

    Nytt linjediagram
    För att göra diagrammet mer interaktivt har jag ändrat så det går justera företagens datum i linjediagrammet. Finns både en slider för datum samt även knappar för olika tidsperioder

--- react-app/src/views/line charts/Line Chart.js
+++ react-app/src/views/line charts/Line Chart.js
@@ -1,117 +1,134 @@
-import React, { Component } from 'react';
-import CanvasJSReact from '../../assets/canvasjs.react';
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
- 
-var dataPoints =[];
-var dataPoints1 =[];
-var dataPoints2 =[];
+import React, { Component } from "react";
+import CanvasJSReact from "../../assets/canvasjs.stock.react";
+var CanvasJSStockChart = CanvasJSReact.CanvasJSStockChart;
+
 class LineChart extends Component {
-	render() {
-		const options = {
-			animationEnabled: true,
-			theme: "light2", // "light1", "dark1", "dark2"
-			title:{
-				text: "Stock Line Chart"
-			},
-			axisY: {
-				title: "Stock Price",
-				includeZero: false,
-				labelFontSize: 20,
-			},
-			axisX: {
-				title: "Datapoints",
-				interval: 200,
-			},
 
-			legend: {
-				cursor: "pointer",
-				fontSize: 16,
-			},
-			toolTip: {
-				enabled: true,
-				shared: true,
-				animationEnabled: true,
-			},
+	constructor(props) {
+		super(props);
+		this.state = { dataPoints: [], isLoaded: false };
+		//this.state = { dataPoints1: [], isLoaded: false };
+		//this.state = { dataPoints2: [], isLoaded: false };
+	  }
 
-			data: [{
-				type: "line",
-				showInLegend: true,
-				name: "fb",
-				yValueFormatString: "$####.00",
-				dataPoints: dataPoints,
-			},
-			{
-				type: "line",
-				showInLegend: true,
-				name: "shop",
-				yValueFormatString: "$####.00",
-				dataPoints: dataPoints1,
-			},
-			{
-				type: "line",
-				showInLegend: true,
-				name: "amzn",
-				yValueFormatString: "$####.00",
-				dataPoints: dataPoints2,
-			}
-		]
-		}
-		
-		return (
-		<div>
-			<h1>React Line Chart</h1>
-			<CanvasJSChart options = {options} 
-			/>
-		</div>
-		);
-	}
-	componentDidMount(){
-		var chart = this.chart;
-		fetch('fb.json')
-		.then(function(response) {
-			return response.json();
-		})
-		.then(function(data) {
-			for (var i = 0; i < data.length; i++) {
-				dataPoints.push({
-					x: data[i].x,
-					y: data[i].y
+	componentDidMount() {
+		fetch('aapl.json')
+		  .then(res => res.json())
+		  .then(
+			(data) => {
+			  var dps = [];
+			  for (var i = 0; i < data.length; i++) {
+				dps.push({
+				  x: new Date(data[i].date),
+				  y: Number(data[i].open)
 				});
+			  }
+			  this.setState({
+				isLoaded: true,
+				dataPoints: dps
+			  });
 			}
-			chart.render();
-		});
+		  )
 
-		fetch('shop.json')
-		.then(function(response) {
-			return response.json();
-		})
-		.then(function(data) {
-			for (var i = 0; i < data.length; i++) {
-				dataPoints1.push({
-					x: data[i].x,
-					y: data[i].y
+		  fetch('fb.json')
+		  .then(res => res.json())
+		  .then(
+			(data) => {
+			  var dps1 = [];
+			  for (var i = 0; i < data.length; i++) {
+				dps1.push({
+				  x: new Date(data[i].date),
+				  y: Number(data[i].open)
 				});
+			  }
+			  this.setState({
+				isLoaded: true,
+				dataPoints1: dps1
+			  });
 			}
-			chart.render();
-		});
+		  )
 
-		fetch('amzn.json')
-		.then(function(response) {
-			return response.json();
-		})
-		.then(function(data) {
-			for (var i = 0; i < data.length; i++) {
-				dataPoints2.push({
-					x: data[i].x,
-					y: data[i].y
+		  fetch('amzn.json')
+		  .then(res => res.json())
+		  .then(
+			(data) => {
+			  var dps2 = [];
+			  for (var i = 0; i < data.length; i++) {
+				dps2.push({
+				  x: new Date(data[i].date),
+				  y: Number(data[i].open)
 				});
+			  }
+			  this.setState({
+				isLoaded: true,
+				dataPoints2: dps2
+			  });
 			}
-			chart.render();
-		});
+		  )
+	  }
 
-		
-	}
+  render() {
+    const options = {
+      animationEnabled: true,
+	  zoomEnabled: true,
+	  title:{
+        text:"React Line Chart"
+      },
+      theme: "light2",
+	  legend: {
+		cursor: "pointer",
+		fontSize: 16,
+	},
+	toolTip: {
+		enabled: true,
+		shared: true,
+	},
+      charts: [{
+          data: [{
+            type: "line",
+			showInLegend: true,
+			name: "Apple",
+			yValueFormatString: "$####.00",
+            dataPoints : this.state.dataPoints
+          },
+		  {
+			type: "line",
+			showInLegend: true,
+			name: "Facebook",
+			yValueFormatString: "$####.00",
+            dataPoints : this.state.dataPoints1
+		  },
+		  {
+			type: "line",
+			showInLegend: true,
+			name: "Amazon",
+			yValueFormatString: "$####.00",
+            dataPoints : this.state.dataPoints2
+		  },
+		],
+      }],
+      navigator: {
+        slider: {
+          minimum: new Date("2018-02-01"),
+          maximum: new Date("2020-10-27")
+        }
+      }
+    };
+    const containerProps  = {
+      width: "100%",
+      height: "600px"
+    };
 
+    return (
+      <div>
+        <CanvasJSStockChart
+          options={options}
+          containerProps  = {containerProps }
+        />
+      </div>
+    );
+  }
+  
 }
 
 export default LineChart;
